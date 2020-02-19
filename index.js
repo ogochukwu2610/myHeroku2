7a71836de3600b3b63838332b528cf6d9d939866
OGOCHUKWU EMELE (1405932) <1405932@rgu.ac.uk>
2020-02-19 12:37:43 +0000

    first file

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var http= require("http");
+const PORT = process.env.PORT || 3000;
+http.createServer(function (request, response) {
+//send the http header
+//HTTP status:200:ok
+//content Type: text/plain
+response.writeHead(200, {'Content-Type': 'text/plain'});
+
+//send the response body as "Hello World"
+response.end('Hello World\n');
+}).listen(PORT, () => {
+	console.log('Our app is running on port ${PORT}');
+});
+
+//console will print the message
+//console.log('Server running at http://127.0.0.1:8080/');
