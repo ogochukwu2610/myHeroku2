642a4e1358a32ab1ba57ccd7e1f73dc7b16552f7
OGOCHUKWU EMELE (1405932) <1405932@rgu.ac.uk>
2020-02-26 12:35:11 +0000

    A new file

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 
 var http= require("http");
+fs= require('fs');
 const PORT = process.env.PORT || 3000;
 http.createServer(function (request, response) {
 //send the http header
 //HTTP status:200:ok
 //content Type: text/plain
-response.writeHead(200, {'Content-Type': 'text/plain'});
-
+response.writeHead(200, {
+	'Content-Type': 'text/plain',
+	'Access-Control-Allow-Origin' : '*'
+	});
+	var readStream = fs.createReadStream(__dirname + 'index.html');
+	//send a message
+	readStream.pipe(response);
 //send the response body as "Hello World"
-response.end('Hello World\n');
+//response.end('Hello World\n');
 }).listen(PORT, () => {
 	console.log('Our app is running on port ${PORT}');
 });
